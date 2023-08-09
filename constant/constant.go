787e3fc49f4bf6ba4a530634c009e419d69f4697
Sandi Suryadi <sandi.suryadi@idas.id>
2023-08-09 11:50:15 +0700

    add english words

--- constant/constant.go
+++ constant/constant.go
@@ -2,8 +2,12 @@ package constant
 
 const (
 	DecimalSeparator = "." 
-	DecimalSeparatorWordID = "koma" 
+	
+	IDDecimalSeparatorWord = "koma" 
 	IDZero = "nol"
+
+	ENDecimalSeparatorWord = "point" 
+	ENZero = "zero"
 )
 
 var (
@@ -14,4 +18,21 @@ var (
 	IDUnits = [...]string{
 		"", "ribu", "juta", "milyar", "triliun", "quadriliun", "quintiliun", "sextiliun", "septiliun", "oktiliun", "noniliun", "desiliun", "undesiliun", "duodesiliun", "tredesiliun", "quattuordesiliun", "quindesiliun", "sexdesiliun", "septendesiliun", "oktodesiliun", "novemdesiliun", "vigintiliun",
 	}
+	
+	ENNumbers = [...]string{
+		"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+	}
+
+	ENDozen = [...]string{
+		"", "eleven", "twelfe", "thirdteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen",
+	}
+
+	ENTens = [...]string{
+		"", "ten", "twenty", "thirdty", "fourty", "fifty", "sixty", "seventy", "eightty", "ninety",
+	}
+
+	ENUnits = [...]string{
+		"", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion",
+	}
+	
 )
--- functions/functions.go
+++ functions/functions.go
@@ -15,11 +15,16 @@ func NumbersToWords(numbers string, lang string) string {
 		integer, decimal := processDecimal(numbers)
 		separator := ""
 		if decimal != "" {
-			separator = " " + C.DecimalSeparatorWordID + " "
+			separator = " " + C.IDDecimalSeparatorWord + " "
 		}
-		res = NumbersToWordsID(integer) + separator + decimalNumbersToWordsID(decimal)
+		res = NumbersToWordsID(integer) + separator + decimalNumbersToWords(decimal, "id")
 	case "en":
-		res = "English not implemented yet"
+		integer, decimal := processDecimal(numbers)
+		separator := ""
+		if decimal != "" {
+			separator = " " + C.ENDecimalSeparatorWord + " "
+		}
+		res = NumbersToWordsEN(integer) + separator + decimalNumbersToWords(decimal, "en")
 	}
 	return res
 }
@@ -36,17 +41,33 @@ func processDecimal(numbers string) (integer, decimal string) {
 	return
 }
 
-func digitToUnit(digit int) string {
+func digitToUnit(digit int, lang string) string {
+	unitsArray := [22]string{}
+	switch strings.ToLower(lang) {
+	case "id":
+		unitsArray = C.IDUnits
+	case "en":
+		unitsArray = C.ENUnits
+	}
+
 	curIndex := digit / 3
 	if curIndex <= maxIndex {
-		return C.IDUnits[curIndex]
+		return unitsArray[curIndex]
 	}
-	return C.IDUnits[maxIndex]
+	return unitsArray[maxIndex]
 }
 
-func numberToText(index int) string {
+func numberToText(index int, lang string) string {
+	numbersArray := [10]string{}
+	switch strings.ToLower(lang) {
+	case "id":
+		numbersArray = C.IDNumbers
+	case "en":
+		numbersArray = C.ENNumbers
+	}
+
 	if index >= 0 && index < len(C.IDNumbers) {
-		return C.IDNumbers[index]
+		return numbersArray[index]
 	}
 	return ""
 }
@@ -77,13 +98,13 @@ func NumbersToWordsID(numbers string) string {
 			if currentNumber != 0 {
 				if modGroup == 0 {
 					/* unit number not zero */
-					result += space + numberToText(currentNumber) + " " + digitToUnit(digitCount)
+					result += space + numberToText(currentNumber, "id") + " " + digitToUnit(digitCount, "id")
 				} else if modGroup == 2 {
 					/* hundred number */
 					if currentNumber == 1 {
 						result += space + "seratus"
 					} else {
-						result += space + numberToText(currentNumber) + " ratus"
+						result += space + numberToText(currentNumber, "id") + " ratus"
 					}
 				} else {
 					/* 10 - 19 number */
@@ -94,28 +115,28 @@ func NumbersToWordsID(numbers string) string {
 							result += space + "sepuluh"
 							/* Proses Next current number */
 							if digitCount != 1 && (int(numbers[i-1]-'0') != 0 || int(numbers[i]-'0') != 0) {
-								result += " " + digitToUnit(digitCount-1)
+								result += " " + digitToUnit(digitCount-1, "id")
 							}
 						} else {
 							if nextNumber == 1 {
 								result += space + "sebelas"
 							} else {
-								result += space + numberToText(nextNumber) + " belas"
+								result += space + numberToText(nextNumber, "id") + " belas"
 							}
 							/* Proses Next current number */
 							if digitCount != 1 {
-								result += " " + digitToUnit(digitCount-1)
+								result += " " + digitToUnit(digitCount-1, "id")
 							}
 						}
 					} else {
 						/* tens number */
-						result += space + numberToText(currentNumber) + " puluh"
+						result += space + numberToText(currentNumber, "id") + " puluh"
 					}
 				}
 			} else {
 				/* unit number not zero */
 				if modGroup == 0 && (int(numbers[i-2]-'0') != 0 || int(numbers[i-1]-'0') != 0) && digitCount != 0 {
-					result += " " + digitToUnit(digitCount)
+					result += " " + digitToUnit(digitCount, "id")
 				}
 			}
 		}
@@ -129,15 +150,80 @@ func NumbersToWordsID(numbers string) string {
 	return strings.TrimSpace(result)
 }
 
-func decimalNumbersToWordsID(angka string) string {
+func decimalNumbersToWords(numbers string, lang string) string {
 	words := []string{}
-	for _, char := range angka {
+	for _, char := range numbers {
 		digit := int(char - '0')
 		if digit == 0 {
-			words = append(words, "nol")
+			words = append(words, C.IDZero)
 		} else {
-			words = append(words, numberToText(digit))
+			words = append(words, numberToText(digit, lang))
 		}
 	}
 	return strings.Join(words, " ")
+}
+
+func NumbersToWordsEN(numbers string) string {
+	numbersLength := len(numbers)
+	numbersMaxIndex := numbersLength - 1
+
+	// Zero number
+	if numbersMaxIndex == 0 && numbers[0] == '0' {
+		return C.ENZero
+	}
+
+	var space string
+	var result string
+
+	i := 0
+	for i != numbersLength {
+
+		digitCount := numbersMaxIndex - i
+		modGroup := digitCount % 3 // [2,1,0]
+		currentNumber := int(numbers[i] - '0')
+
+		if currentNumber != 0 {
+			if modGroup == 0 {
+				/* unit number not zero */
+				result += space + numberToText(currentNumber, "en") + " " + digitToUnit(digitCount, "en")
+			} else if modGroup == 2 {
+				/* hundred number */
+				result += space + numberToText(currentNumber, "en") + " hundred"
+			} else {
+				/* 10 - 19 number */
+				if currentNumber == 1 {
+					i++ // Skip Next number
+					nextNumber := int(numbers[i] - '0')
+					if nextNumber == 0 {
+						result += space + "ten"
+						/* Proses Next current number */
+						if digitCount != 1 && (int(numbers[i-1]-'0') != 0 || int(numbers[i]-'0') != 0) {
+							result += " " + digitToUnit(digitCount-1, "en")
+						}
+					} else {
+						result += space + C.ENDozen[nextNumber]
+						/* Proses Next current number */
+						if digitCount != 1 {
+							result += " " + digitToUnit(digitCount-1, "en")
+						}
+					}
+				} else {
+					/* tens number */
+					result += space + C.ENTens[currentNumber]
+				}
+			}
+		} else {
+			/* unit number not zero */
+			if modGroup == 0 && (int(numbers[i-2]-'0') != 0 || int(numbers[i-1]-'0') != 0) && digitCount != 0 {
+				result += " " + digitToUnit(digitCount, "en")
+			}
+		}
+
+		if i <= 1 {
+			space = " "
+		}
+		i++
+	}
+
+	return strings.TrimSpace(result)
 }
